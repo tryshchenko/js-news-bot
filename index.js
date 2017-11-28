cc8caee26907b8aeb4fbd9225ccfba0856049820
Alex Pasechnik <Alexander.Pasechnik@dataart.com>
2017-11-28 16:11:09 +0200

    Added Frontend Focus parser

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const Engine = require('./engine');
 const JsweeklyParser = require('./parsers/jsweekly.parser');
 const NewsjsParser = require('./parsers/newsjs.parser');
 const ChromeReleasesParser = require('./parsers/chrome-releases.parser');
+const FrontendFocusParser = require('./parsers/frontend-focus.parser');
 
 const parsers = [
 	JsweeklyParser,
 	NewsjsParser,
-	ChromeReleasesParser
+	ChromeReleasesParser,
+	FrontendFocusParser
 ];
 
 const engine = new Engine(config, parsers).init();
---
+++ parsers/frontend-focus.parser.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const BaseParser = require('./base.parser');
+const ffweeklyFeed = 'https://frontendfoc.us/issues';
+const absoluteLinkBody = 'https://frontendfoc.us/';
+
+class FrontendFocusParser extends BaseParser {
+  getAlias() {
+    return 'frontend_focus_parser';
+  }
+
+  parse() {
+    request(ffweeklyFeed, (err, resp, body) => {
+      this.document = this.getDOM(body);
+      const issues = [].slice.call(this.document.querySelectorAll('.issue'));
+      issues.forEach((issue, index) => {
+        const linkHtml = issue.querySelector('a');
+        const link = `${absoluteLinkBody}${linkHtml.getAttribute('href')}`;
+        const title = `New FrontEnd Focus rolled out: ${linkHtml.innerHTML}`;
+        const message = [title, link].join('\n\n');
+
+        this.collection.find({uid: link}).toArray((err, res) => {
+          if (res.length === 0) {
+            this.handleRecord(message, link);
+          }
+        });
+      });
+    });
+  }
+}
+
+module.exports = FrontendFocusParser;
