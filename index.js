7ac34f3aa69c975d2e607c6782b467ba56d8387d
Alex Pasechnik <karazyabko@gmail.com>
2017-11-28 23:07:27 +0200

    Added Frontend Front parser

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const NewsjsParser = require('./parsers/newsjs.parser');
 const ChromeReleasesParser = require('./parsers/chrome-releases.parser');
 const FrontendFocusParser = require('./parsers/frontend-focus.parser');
 const NodeWeeklyParser = require('./parsers/nodeweekly.parser');
+const FrontendFrontParser = require('./parsers/frontend-front.parser');
 
 const parsers = [
 	JsweeklyParser,
 	NewsjsParser,
 	ChromeReleasesParser,
 	FrontendFocusParser,
-	NodeWeeklyParser
+	NodeWeeklyParser,
+  FrontendFrontParser
 ];
 
 const engine = new Engine(config, parsers).init();
---
+++ parsers/frontend-front.parser.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const BaseParser = require('./base.parser');
+const frontFrontFeed = 'https://frontendfront.com/?s=javascript';
+
+class FrontendFrontParser extends BaseParser {
+  getAlias() {
+    return 'frontend_front_parser';
+  }
+
+  parse() {
+    request(frontFrontFeed, (err, resp, body) => {
+      this.document = this.getDOM(body);
+      const issues = [].slice.call(this.document.querySelectorAll('.stories ul li'));
+      issues.forEach((issue, index) => {
+        const titleLink = issue.querySelector('.story-title h2 a');
+        const link = titleLink.getAttribute('href');
+        const title = titleLink.getAttribute('title');
+        const message = [title, link].join('\n\n');
+
+        this.collection.find({uid: link}).toArray((err, res) => {
+          if (res.length === 0) {
+            this.handleRecord(message, link);
+          }
+        });
+      });
+    });
+  }
+}
+
+module.exports = FrontendFrontParser;
